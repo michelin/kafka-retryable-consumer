186a3fc84d45a3800b79c0bcbb21dada4f0a0abc
Julien BRUNET <julien.brunet2@michelin.com>
2025-07-15 16:22:40 +0200

    fix unit tests

--- retryable-consumer-core/src/test/java/com/michelin/kafka/test/unit/RetryableConsumerTest.java
+++ retryable-consumer-core/src/test/java/com/michelin/kafka/test/unit/RetryableConsumerTest.java
@@ -125,8 +125,8 @@ class RetryableConsumerTest {
                         Collections.emptyMap(),
                         Collections.singletonMap(record1TopicPartition, new OffsetAndMetadata(1L))));
 
-        retryableConsumer.listen(r -> recordProcessorNoError.processRecord(r));
-        verify(kafkaConsumer, timeout(10000).atLeast(1)).poll(any());
+        retryableConsumer.listenAsync(r -> recordProcessorNoError.processRecord(r));
+        verify(kafkaConsumer, timeout(30000).atLeast(1)).poll(any());
         verify(recordProcessorNoError, timeout(10000).times(1)).processRecord(any());
 
         Assertions.assertEquals(
